18169c5736b3b159f06c8e5dbec02da5ffb3cbc8
bashao <bashao.lwq@taobao.com>
2013-04-26 01:48:54 +0800

    brix权威指南

--- components/showcase/index.js
+++ components/showcase/index.js
@@ -3,15 +3,28 @@ KISSY.add("components/showcase/index", function(S, Brick, Pagelet) {
         ShowCase.superclass.constructor.apply(this, arguments);
     }
     ShowCase.ATTRS = {
+        // 需要获取的页面路径
         path : {
             value : ''
         },
+        basepath : {
+            value : ''
+        },
+        //渲染完页面需要什么样的替换方式
+        // 默认为 ‘easeNone’ , 动画平滑函数, 
+        // 可取值 “easeNone”,”easeIn”,”easeOut”,”easeBoth”,”easeInStrong”, 
+        // “easeOutStrong”,”easeBothStrong”,”elasticIn”,”elasticOut”, 
+        // “elasticBoth”,”backIn”,”backOut”,”backBoth”, “bounceIn”,”bounceOut”,
+        // ”bounceBoth”. 
         mod : {
             value : ''
         },
+        //需要绑定动作的元素tag
+        //用来出发click事件
         eltag : {
             value : ''
         },
+        //ajax获取页面代码后渲染的位置
         place : {
             value : ''
         }
@@ -23,11 +36,13 @@ KISSY.add("components/showcase/index", function(S, Brick, Pagelet) {
             var mod = self.get('mod');
             if(!mod){
                 self._renderUI(self.get('path'));
-            }else{
+            }else if(mod==='S'){
                 self._sliderUI();
+            }else if(mod==='M'){
+                self._menu();
             }   
         },
-        _renderUI : function(path,target){
+        _renderUI : function(path,cb){
             var self = this;
             if(path){
                 S.ajax({
@@ -43,10 +58,8 @@ KISSY.add("components/showcase/index", function(S, Brick, Pagelet) {
                             self.pagelet = new Pagelet({
                                  tmpl: '#'+S.one(el).attr('id')
                             });
-                            if(self.get('transition')){
-                                self.pagelet.ready(function(){
-                                    self._transition(target,self.get('el'),el);
-                                });
+                            if(cb){
+                                cb.call();
                             }
                         }
                     }
@@ -63,8 +76,18 @@ KISSY.add("components/showcase/index", function(S, Brick, Pagelet) {
                     n = S.one(n);
                     if(n && n.attr('path')){
                         var path = n.attr('path');
-                        n.on('click',function(){
-                            self._renderUI(path,n);
+                        n.on('click',function(e){
+                            e.halt();
+                            self._renderUI(path,function(){
+                                if(hljs){
+                                    hljs.initHighlighting();
+                                }
+                                if(self.get('transition')){
+                                    self.pagelet.ready(function(){
+                                        self._transition(n,self.get('el'),el);
+                                    });
+                                }
+                            });
                         });
                     }
                 });
@@ -88,14 +111,60 @@ KISSY.add("components/showcase/index", function(S, Brick, Pagelet) {
                 width: '150px'
             });
             
-            var anim = S.Anim(l,
-                {
+            var anim = S.Anim(l,{
                     'opacity': '0'
-                },1,
-                transmode,function(){
+                },1,transmode,function(){
                     l.hide();
-            });
+                }
+            );
             anim.run();
+        },
+        _menu : function(){
+            var self = this;
+            var eltag = self.get('eltag');
+            var el = self.get('el');
+            var defaul = '';
+            var ha = window.location.hash; 
+            if(ha){
+                var h = window.location.hash;
+                h = h.split('#!')[1];
+                if(h.indexOf('&')>-1){
+                    h = h.split('&')[0];
+                }
+                var base = self.get('basepath');
+                if(base && h){
+                    defaul = base+h+'.html';
+                }
+            }
+            self._renderUI(defaul);
+            if(el && eltag){
+                var all = el.all(eltag);
+                S.each(all, function(n){
+                    n = S.one(n);
+                    if(n.attr('href')===ha){
+                        n.parent().addClass('current');
+                    }
+                    n.on('click',function(e){
+                        var tar = e.target;
+                        var cur = el.one('.current');
+                        if(cur){
+                            cur.removeClass('current');
+                        }
+                        if(tar){
+                            S.one(tar).parent().addClass('current');
+                        }
+                    });
+                });
+            }
+            S.one(window).on('hashchange', function() {
+                var h = window.location.hash;
+                h = h.split('#!')[1];
+                if(h.indexOf('&')>-1){
+                    h = h.split('&')[0];
+                }
+                var base = self.get('basepath');
+                self._renderUI(base+h+'.html');
+            });
         }
     });
 

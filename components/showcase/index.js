43e235e2c94476c4492fda1962fc4f71c64cccae
bashao <bashao.lwq@taobao.com>
2013-04-26 16:54:09 +0800

    新的修正

--- components/showcase/index.js
+++ components/showcase/index.js
@@ -27,6 +27,10 @@ KISSY.add("components/showcase/index", function(S, Brick, Pagelet) {
         //ajax获取页面代码后渲染的位置
         place : {
             value : ''
+        },
+        //动画还原触发元素
+        goback : {
+            value : ''
         }
     };
     S.extend(ShowCase, Brick, {
@@ -84,7 +88,7 @@ KISSY.add("components/showcase/index", function(S, Brick, Pagelet) {
                             self._renderUI(path,function(){
                                 if(self.get('transition')){
                                     self.pagelet.ready(function(){
-                                        self._transition(n,self.get('el'),el);
+                                        self._transition(n,self.get('el'));
                                     });
                                 }
                             });
@@ -92,31 +96,62 @@ KISSY.add("components/showcase/index", function(S, Brick, Pagelet) {
                     }
                 });
             }
+            var gb = self.get('goback');
+            if(gb){
+                gb = S.one(gb);
+                gb.detach('click');
+                gb.on('click',function(e){
+                    e.halt();
+                    self._transitionback(gb,S.one(self.get('el')),S.one(self.get('place')));
+                });
+            }
         },
         // 切换2个div，t--点击的元素 l--展示组件列表的div c--是组件展示div
-        _transition : function(t,l,c){
+        _transition : function(t,l){
             var self = this;
+            var gb = self.get('goback');
+            if(gb) S.one(gb).show();
             //动画类型，一般标识动画的展现效果。
             var transmode = self.get('transtype') || 'easeOut';
-            var clonet = S.DOM.create('<div></div>');
+            var clonet = S.DOM.create('<div id="clonecomp"></div>');
             l.parent().append(clonet);
             clonet = S.one(clonet);
             clonet.addClass('font-Arch');
             clonet.html(t.html());
             clonet.css({
                 position:'absolute',
-                top:'0px',
-                left:'0px',
+                top:'-37px',
+                left:'35px',
                 'z-index': 3,
                 width: '150px'
             });
-            
+            S.one(self.get('place')).show();
             var anim = S.Anim(l,{
                     'opacity': '0'
-                },1,transmode,function(){
+                },0.6,transmode,function(){
                     l.hide();
                 }
             );
+            anim.run();
+        },
+        _transitionback : function(n,l,c){
+            var temp = S.one('#clonecomp');
+            if(temp) temp.remove();
+            n.hide();
+            l.show();
+            var anim0 = S.Anim(c,{
+                'left' :  '1000px'
+            },0.6,'easeOut',function(){
+                c.html('');
+                c.hide();
+                c.css('left','0px');
+            });
+            var anim = S.Anim(l,{
+                    'opacity': '1'
+            },0.8,'easeIn',function(){
+                l.show();
+            });
+            anim0.run();
             anim.run();
         },
         _menu : function(){
